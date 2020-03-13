b8a82bb15652201c561cee4a40b4b39b02d8d6b1
Jeremiah <jeremiahteee@gmail.com>
2020-03-13 18:40:43 -0400

    Comment to trigger Jenkins build

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -28,6 +28,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(proxyBeanMethods = false)
 public class PetClinicApplication {
 
+	//Adding comment to trigger Jenkins build
 	public static void main(String[] args) {
 		SpringApplication.run(PetClinicApplication.class, args);
 	}
